7766c6a0c6e0404c8329be42e7d8f3d18b406cc3
crmgunter <crmgunter@gmail.com>
2018-04-11 15:54:59 -0400

    post and show routes working for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-
+//GET ROUTE
 router.get('/', (req, res) => {
     User.find()
     .then(users => {
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
         console.log(err)
     })
 })
-
+//POST ROUTE
 router.post('/', (req, res) => {
     const newUser = new User({
         username: req.body.username,
@@ -20,6 +20,16 @@ router.post('/', (req, res) => {
     })
     newUser.save().then(savedUser => {
         res.redirect(`/api/users`)
+    })
+})
+// SHOW ROUTE
+router.get('/:id', (req, res) => {
+    const userId = req.params.id
+    const user = User.findById(userId).then((user) => {
+        res.json(user)
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
     })
 })
 
