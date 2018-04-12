bbd877dd9d8e14abbc0e65d47a55d4466b5685b7
crmgunter <crmgunter@gmail.com>
2018-04-12 17:16:47 -0400

    got oauth working, need to figure out how to connect it to my app

--- client/src/components/User/Home.js
+++ client/src/components/User/Home.js
@@ -1,47 +1,69 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Spotify from 'spotify-web-api-js'
-import User from './User'
-import NewUserForm from './NewUserForm'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Spotify from "spotify-web-api-js";
+import User from "./User";
+import NewUserForm from "./NewUserForm";
 
+const SpotifyWebApi = new Spotify();
 
 class Home extends Component {
-
-    state = {
-        users: [],
-        form: false
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      users: [],
+      form: false
+    };
+    if (params.access_token) {
+      SpotifyWebApi.setAccessToken(params.access_token);
     }
+  }
 
-    componentDidMount() {
-        this.getAllUsers()
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
+    return hashParams;
+  }
 
-    getAllUsers = async () =>{
-        const res = await axios.get("/api/users")
-        console.log(res.data)
-        this.setState({ users: res.data})
-    }
+  componentDidMount() {
+    this.getAllUsers();
+  }
 
-    toggleForm = () => {
-        this.setState({ form: !this.state.form })
-    }
+  getAllUsers = async () => {
+    const res = await axios.get("/api/users");
+    console.log(res.data);
+    this.setState({ users: res.data });
+  };
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.toggleForm}>Create New User</button>
-                {this.state.form? (<NewUserForm getAllUsers={this.getAllUsers}/>) : null }
-                {this.state.users.map(user => (
-                    <div key={user._id}>
-                    <Link to={`users/${user._id}`}>{user.username}</Link>
-                    {user.location}
-                    <img src={user.image} alt="user image"/>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+  toggleForm = () => {
+    this.setState({ form: !this.state.form });
+  };
+
+  render() {
+    return (
+      <div>
+          <a href="http://localhost:8888"><button>Login with Spotify</button></a>
+        <button onClick={this.toggleForm}>Create New User</button>
+        {this.state.form ? (
+          <NewUserForm getAllUsers={this.getAllUsers} />
+        ) : null}
+        {this.state.users.map(user => (
+          <div key={user._id}>
+            <Link to={`users/${user._id}`}>{user.username}</Link>
+            {user.location}
+            <img src={user.image} alt="user image" />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Home;
