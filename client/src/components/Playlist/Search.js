5f0c54e46a7d2a76b017b6dee585dcecf3f8e3b5
crmgunter <crmgunter@gmail.com>
2018-04-15 19:49:55 -0400

    got search bar console logging search results. ALWAYS MAKE SURE TO NEVER FORGET THE ? BEFORE A QUERY YOU DUMMY

--- client/src/components/Playlist/Search.js
+++ client/src/components/Playlist/Search.js
@@ -15,7 +15,7 @@ class Search extends Component {
   handleSubmit = event => {
     event.preventDefault();
     const token = localStorage.getItem("token");
-    const query = `q=${this.state.search}&type=artist,track,album,playlist`
+    const query = `?q=:${this.state.search}&type=artist,track,album,playlist`
     fetch(`https://api.spotify.com/v1/search/${query}`, {
       headers: { Authorization: `Bearer ${token}` }
     })
