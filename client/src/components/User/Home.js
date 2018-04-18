abcd6fdbb8351d1152bb462cd3c839717936ca5c
crmgunter <crmgunter@gmail.com>
2018-04-18 16:23:26 -0400

    please work

--- client/src/components/User/Home.js
+++ client/src/components/User/Home.js
@@ -89,7 +89,7 @@ class Home extends Component {
           <button onClick={() => {
             window.location = window.location.href.includes('localhost') 
             ? 'http://localhost:8888' 
-            : 'https://cgfinalproject.herokuapp.com/login'
+            : 'https://cgfinalproject.herokuapp.com'
           }}>Login with Spotify</button>
         {/* </a> */}
   <p>WORK!!!</p>
