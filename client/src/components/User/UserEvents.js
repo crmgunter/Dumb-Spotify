96df9480f56d89a8b9bc22ec12a25eb9097457e1
crmgunter <crmgunter@gmail.com>
2018-04-20 00:04:43 -0400

    got a return of venues for hardcoded artist api call

--- client/src/components/User/UserEvents.js
+++ client/src/components/User/UserEvents.js
@@ -1,53 +1,63 @@
 import React, { Component } from "react";
 class UserEvents extends Component {
   state = {
-        events: [{
-            events: [{
-                name: ''
-            }]
-        }]
+      events: [{
+          venue: {
+              name: ''
+          }
+      }]
   };
 
   componentDidMount() {
-    this.getTopArtistsEvents();
+    // this.getTopArtistsEvents();
+    this.getEvents();
   }
 
-  getTopArtistsEvents = () => {
-    let params = this.props.getHashParams();
-    let accessToken = params.access_token;
+  //   getTopArtistsEvents = () => {
+  //     let params = this.props.getHashParams();
+  //     let accessToken = params.access_token;
 
-    fetch("https://api.spotify.com/v1/me/top/artists", {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
+  //     fetch("https://api.spotify.com/v1/me/top/artists", {
+  //       headers: { Authorization: `Bearer ${accessToken}` }
+  //     })
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         //   this.setState({ userTop: data })
+  //         const artist = data;
+  //         const allEvents = [];
+  //         artist.items.map(artistName =>
+  //           fetch(
+  //             `https://rest.bandsintown.com/artists/${artistName.name}/events?app_id=698b530f4ba8c70ef531c9eae82e8204`
+  //           )
+  //             .then(response => response.json())
+  //             .then(data => {
+  //               const artistEvent = data;
+  //               if (artistEvent) {
+  //                 allEvents.push(artistEvent);
+  //               }
+  //             })
+  //         );
+  //         this.setState({ events: allEvents })
+  //         console.log(this.state)
+  //       });
+  //   };
+
+  getEvents = () => {
+    fetch(
+      `https://rest.bandsintown.com/artists/ttng/events?app_id=698b530f4ba8c70ef531c9eae82e8204`
+    )
       .then(response => response.json())
-      .then(data => {
-        //   this.setState({ userTop: data })
-        const artist = data;
-        const allEvents = [];
-        artist.items.map(artistName =>
-          fetch(
-            `https://rest.bandsintown.com/artists/${artistName.name}/events?app_id=698b530f4ba8c70ef531c9eae82e8204`
-          )
-            .then(response => response.json())
-            .then(data => {
-              const artistEvent = data;
-              if (artistEvent) {
-                allEvents.push(artistEvent);
-              }
-            })
-        );
-        this.setState({ events: allEvents })
-        console.log(this.state)
-      });
+    //   .then(data => console.log(data))
+      .then(data => this.setState({ events: data}))
   };
 
   render() {
     return (
       <div>
         <h1>user Events</h1>
-        {this.state.events.map((event) => {
-            event._embedded? (<div>{event.events}</div>) : null
-        })}
+            {this.state.events.map(event => (
+                <div>{event.venue.name}</div>
+            ))}
       </div>
     );
   }
