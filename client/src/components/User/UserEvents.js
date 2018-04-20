2ed1ef9123ee90e2063c42c2a26056e046571e41
crmgunter <crmgunter@gmail.com>
2018-04-19 21:21:37 -0400

    I GOT A REALLY WEIRD NESTED API CALL WORKING I AM A GOD! I AM A FUCKING GOD I TELL YOU! A GODgit add -A! Got an api call to spotify to loop through user's top artists, then plugged it into another api call to ticket master to loop through a bunch of api calls and returns any events that includes the artist's name

--- client/src/components/User/UserEvents.js
+++ client/src/components/User/UserEvents.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 class UserEvents extends Component {
   state = {
-      userTop: {
-          items: [{}]
-      },
     events: {
       _embedded: {
         events: [
@@ -17,9 +14,8 @@ class UserEvents extends Component {
 
   componentDidMount() {
     this.getTopArtists();
-    this.getEvents()
+    // this.getEvents();
   }
-
 
   getTopArtists = () => {
     let params = this.props.getHashParams();
@@ -29,25 +25,48 @@ class UserEvents extends Component {
       headers: { Authorization: `Bearer ${accessToken}` }
     })
       .then(response => response.json())
-      .then(data => this.setState({ userTop: data }));
+      .then(data => {
+        //   this.setState({ userTop: data })
+        const artist = data;
+        console.log(artist);
+        const artistResponse = artist.items.map(artistName =>
+          fetch(
+            `https://app.ticketmaster.com/discovery/v2/events.json?apikey=udwfh91BymplrOZLNeV8nhVhMd032Xrn&keyword=${
+              artistName.name
+            }&startDateTime=2018-04-19T18:35:00Z&city=atlanta`
+          )
+            .then(response => response.json())
+            .then(data => {
+              // console.log(data._embedded)
+              const event = data._embedded;
+              if (event) {
+                this.setState({ events: event });
+              }
+            })
+        );
+      });
   };
 
-  getEvents = () => {
-    fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?apikey=udwfh91BymplrOZLNeV8nhVhMd032Xrn&keyword=ttng&startDateTime=2018-04-19T18:35:00Z&city=atlanta`
-    )
-      .then(response => response.json())
-      .then(data => this.setState({ events: data }))
-      .then(data => console.log(this.state.events))
-  };
+  //   getEvents = () => {
+  //     fetch(
+  //       `https://app.ticketmaster.com/discovery/v2/events.json?apikey=udwfh91BymplrOZLNeV8nhVhMd032Xrn&keyword=ttng&startDateTime=2018-04-19T18:35:00Z&city=atlanta`
+  //     )
+  //       .then(response => response.json())
+  //       .then(data => this.setState({ events: data }))
+  //       .then(data => console.log(this.state.events));
+  //   };
 
   render() {
     return (
       <div>
         <h1>user Events</h1>
-        {this.state.events._embedded.events.map(event => (
+        {/* {this.state.events._embedded? (
+        <div>
+            {this.state.events._embedded.events.map(event => (
           <div key={event.name}>{event.name}</div>
         ))}
+        </div>
+        ) : null } */}
       </div>
     );
   }
